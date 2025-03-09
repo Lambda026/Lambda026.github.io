ac613b69737bc6e50975e6099ee8e192d60da57b
Lambda <3222707117@qq.com>
2025-03-09 20:30:54 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
